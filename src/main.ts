701e9a56aeb6ac13c1337343c1ee90cf52d93897
Nithack <andreynithack@gmail.com>
2021-07-14 23:37:44 -0300

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as helmet from 'helmet';
 async function bootstrap() {
   const logger = new Logger('Main Publication Service');
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   await app.listen(3000, () => {
     logger.log('Server Listem http://localhost:3000');
   });
